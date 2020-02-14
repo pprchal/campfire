c7bf7ea5826f348e55656f70db814454718c59a7
Pavel Prchal <prchalp@gmail.com>
2020-02-14 18:21:28 +0100

    html funguje

--- project/renderers/html/HtmlRenderer.py
+++ project/renderers/html/HtmlRenderer.py
@@ -12,9 +12,14 @@ from project.renderers.BaseRenderer import BaseRenderer
 class HtmlRenderer(BaseRenderer):
     def __init__(self, config: Config, song: Song):
         super().__init__(config, song)
-        self.row = 0
         self.out = ''
 
+    @property
+    def chordSymbols(self):
+        return {
+            'b': '&#9837;',
+            '#': '&#9839;'
+        }
 
     def renderMetadata(self):
         """
@@ -31,85 +36,69 @@ class HtmlRenderer(BaseRenderer):
         """
         self.drawElement(self.song.getMeta('title'), 'h1')
         self.drawElement(self.song.getMeta('artist'), 'h2')
-
-        # line ---------------------
-        # self.pdf.line(self.pdf.l_margin, yLine, self.pdf.w - self.pdf.r_margin, yLine)
-        # custom metadata (time: 3/4,....)
         self.renderMetadata()
 
 
-    def renderSection(self, section : Section):
-        """
-        render single section
-        """
-        # ...and content
-        self.renderSectionTitle(section)
-        for line in section.lines:
-            self.renderSectionLine(line, section)
+    def renderSection(self, section: Section):
+        htmlSection = '<div>\n'
+        htmlSection += '<div>{}</div>'.format(str(section.n + 1))
+        nSectionLine = 0
 
-    
+        for line in section.lines:
+            htmlSection += '<table>\n'
+            htmlChLine, htmlLyLine, hasChord = self.renderSectionLine(line, nSectionLine, section)
+            nSectionLine = nSectionLine + 1
+
+            if hasChord:
+                htmlSection += '<tr>{}</tr>\n'.format(htmlChLine)
+            htmlSection += '<tr>{}</tr>\n'.format(htmlLyLine)
+            htmlSection += '</table>\n'
+
+        return htmlSection + '</div>\n'
+
+
+    def renderSectionLine(self, sectionLine: SectionLine, nSectionLine: int, section: Section):
+        """
+        render song line (chords + lyrics)
+        """
+        htmlChLine = ''
+        htmlLyLine = ''
+        hasChord = False
+        nMeasure = 0
+
+        for measure in sectionLine.measures:
+            htmlChLine += '<td>{}</td>'.format(self.formatChord(measure.chord))
+            htmlLyLine += '<td>{}</td>'.format(self.formatLyrics(measure.lyrics))
+
+            if not measure.chord == '':
+                hasChord = True
+            nMeasure = nMeasure + 1
+
+        return (htmlChLine, htmlLyLine, hasChord)
+
+
     def renderSectionTitle(self, section: Section):
         """
         render section title
         """
-        self.drawText(self.formatSectionTitle(section), 'h4')
-        self.row = self.row + 1
+        self.drawElement(self.formatSectionTitle(section), 'h4')
 
     
     def formatChord(self, txtChord:str):
         """
         format chord
         """
-        return '[' + super().formatChord(txtChord) + ']'
+        htmlChord = ''
+        for c in txtChord:
+            if c in self.chordSymbols:
+                htmlChord += self.chordSymbols[c]
+            else:
+                htmlChord += c
+
+        return htmlChord
 
 
-    def renderSectionLine(self, sectionLine : SectionLine, section : Section):
-        """
-        render song line (chords + lyrics)
-        """
-        isLyricsRendered = 0
-        isChordRendered = 0
-        renderedWidth = 0
-
-        l1 = ''
-        l2 = ''
-
-        for i in range(0, len(sectionLine.measures)):
-            width = 0
-            measure = sectionLine.measures[i]
-            # format
-            chord = self.formatChord(measure.chord)
-            lyrics = self.formatLyrics(measure.lyrics)
-
-            k = max(len(chord), len(lyrics))
-            chord = chord.ljust(k)
-            lyrics = lyrics.ljust(k)
-
-            # chord
-            if not chord.strip() == '':
-                l1 = l1 + chord
-                isChordRendered = 1
-
-            # lyrics
-            if not lyrics.strip() == '':
-                l2 = l2 + lyrics
-                isLyricsRendered = 1
-
-            renderedWidth = renderedWidth + width
-
-        self.drawText(l1, FontStyles.CHORD)
-        self.drawText('\n', FontStyles.LYRICS)
-        self.drawText(l2, FontStyles.LYRICS)
-        self.drawText('\n', FontStyles.LYRICS)
-        # overflows
-        # if renderedWidth > self.colWidth:
-        #     print('Line:{} [{}] is too wide! please wrap it'.format(sectionLine.linePosition, sectionLine.rawLine))
-
-        renderedRows =  (isChordRendered + isLyricsRendered)
-        self.row = self.row + renderedRows
-
-
-    def drawText(self, text:str, fontStyle:FontStyles):
+    def drawText(self, text:str):
         """
         draw text
         """
@@ -127,11 +116,11 @@ class HtmlRenderer(BaseRenderer):
         """
         render song in one reusable html block 
         """
-        self.out = "<html><body>"
+        self.drawText('<head><meta charset=\"utf-8\" /></head>')
         self.renderSongHeader()
         for section in self.song.sections:
-            self.renderSection(section)
-        self.out = self.out + "</body></html>"
+            self.drawText(self.renderSection(section))
+        self.drawText('</body></html>')
         return self.out.encode()
 
 
